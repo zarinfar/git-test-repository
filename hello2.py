5bea4cd63b050dd9a9e31399b0e398d4ff5155ea
zarinfar <maminzarinfar1001@gmail.com>
2016-01-27 18:01:39 +0330

    18:01

---
+++ hello2.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/<name>')
+def index(name):
+	return 'hello %s' %name 
+
+@app.route('/<namea>')
+def user(namea):
+	return 'hello %s' %namea 
+if __name__=='__main__':
+	app.run(debug=True)
