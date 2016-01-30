e5cc276885500ff44be5e214ceea257f0b8d0faf
zarinfar <maminzarinfar1001@gmail.com>
2016-01-30 18:23:19 +0330

    94.11.10

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask , make_response
 app=Flask(__name__)
 
 @app.route('/<name>')
 def index(name):
 	return 'hello %s' %name 
 
-@app.route('/<namea>')
-def user(namea):
-	return 'hello %s' %namea 
+@app.route('/')
+def badrequest():
+	return '<h1>bad request</h1>',400
+
+@app.route('/response/')
+def response():	
+	response=make_response('<h1>This document carries a cookie!</h1>')
+	response.set_cookie('answer','10')
+	return response
+
 if __name__=='__main__':
 	app.run(debug=True)
