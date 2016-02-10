29da4e986e37d118dfddcddfff4b06b6e85b3cdb
zarinfar <maminzarinfar1001@gmail.com>
2016-02-10 18:02:41 +0330

    94.11.akharin commit

--- form/hello.py
+++ form/hello.py
@@ -1,5 +1,4 @@
-from flask import Flask, make_response
-from flask import redirect, abort, render_template
+from flask import Flask, render_template, session, redirect, url_for, request
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
@@ -13,6 +12,10 @@ class Give_data(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+
+class SessionForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 class UserInfo(Form):
     first_name = StringField('username :')
@@ -31,18 +34,17 @@ def page_not_found(error_this_name_is_optional):
 def internal_server_error(error_this_name_is_optional):
     return render_template('500.html'), 500
 
-
-@app.route('/input_data_from_form/', methods=['GET', 'POST'])
-# @app.route('/input_data_from_form/', methods=['POST'])
-def input_data_from_form():
-    name = None
-    form = Give_data()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-
-    return render_template('form.html', form=form, name=name)
-
+#
+# @app.route('/input_data_from_form/', methods=['GET', 'POST'])
+# def input_data_from_form():
+#     name = None
+#     form = Give_data()
+#     if form.validate_on_submit():
+#         name = form.name.data
+#         form.name.data = ''
+#
+#     return render_template('form.html', form=form, name=name)
+#
 
 @app.route('/user_info_input_data/', methods=['GET', 'POST'])
 def user_info():
@@ -65,6 +67,17 @@ def user_info():
                            first_name=first_name,
                            last_name=last_name,
                            phone_number=phone_number)
+
+
+@app.route('/SessionForm/', methods=['GET', 'POST'])
+def hello():
+    form = SessionForm()
+    if request.method == 'POST':
+        session['name'] = form.name.data
+        return redirect(url_for('hello'))
+
+    return render_template('SessionForm.html', form2=form, name=session.get('name'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
